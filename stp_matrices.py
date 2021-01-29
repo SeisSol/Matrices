f0d8bb4d5b90b0f3bb42f70b211bf6609a57873e
Sebastian Wolf <wolf.sebastian@in.tum.de>
2021-01-29 13:28:13 +0100

    Include Space Time Predictor

---
+++ stp_matrices.py
@@ -0,0 +1,63 @@
+import numpy as np
+import quadpy as qp
+
+import basis_functions
+import dg_matrices
+
+### generates matrices which are needed for the space time predictor:
+### Z
+### value at 0
+### time integrated
+
+class stp_generator:
+    def __init__(self, o):
+        self.order = o
+
+        #TODO: Choose a quadrature formula according to the order
+        self.generator = basis_functions.BasisFunctionGenerator1D(self.order)
+        self.scheme = qp.line_segment.gauss_legendre(15)
+        self.geometry = [0.0, 1.0]
+        self.dg_generator = dg_matrices.dg_generator(self.order, 1)
+
+    def eval_at_zero(self):
+        number_of_basis_functions = self.generator.number_of_basis_functions()
+        w = np.zeros((number_of_basis_functions,))
+    
+        for i in range(number_of_basis_functions):
+            w[i] = self.generator.eval_basis(0, i)
+
+        S = self.dg_generator.mass_matrix()
+        return np.linalg.solve(S, w)
+
+    def W(self):
+        number_of_basis_functions = self.generator.number_of_basis_functions()
+        W = np.zeros((number_of_basis_functions,number_of_basis_functions))
+    
+        for i in range(number_of_basis_functions):
+            for j in range(number_of_basis_functions):
+                W[i,j] = self.generator.eval_basis(1, i) * \
+                    self.generator.eval_basis(1, j)
+        return W
+
+    def Z(self):
+        S = self.dg_generator.mass_matrix()
+        W = self.W()
+        K_t = self.dg_generator.stiffness_matrix(0)
+
+        return np.linalg.solve(S, W-K_t)
+
+    def time_int(self):
+        number_of_basis_functions = self.generator.number_of_basis_functions()
+        t = np.zeros((number_of_basis_functions,))
+        t[0] = 1
+
+        return t
+
+if __name__ == '__main__':
+    generator = stp_generator(5)
+    print(generator.eval_at_zero())
+    print(generator.Z())
+    print(generator.time_int())
+
+
+    
