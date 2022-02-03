4c7578cdbb2f43607659062e29da64b8c64ef13f
Sebastian Wolf <wolf.sebastian@in.tum.de>
2022-02-03 17:50:44 +0100

    JSON Writer

--- seissol_matrices/json_io.py
+++ seissol_matrices/json_io.py
@@ -9,6 +9,32 @@ def read_json(filename):
     file_txt = " ".join([line for line in lines])
     return json.loads(file_txt)
 
+def write_json(content, filename):
+    with open(filename, "w+") as f:
+        json.dump(content, f)
+
+
+def create_empty_json_file(filename):
+    write_json([], filename)
+
+
+def write_matrix(matrix, matrixname, filename):
+    if not os.path.exists(filename):
+        create_empty_json_file(filename)
+
+    matrix_list = read_json(filename)
+    assert(isinstance(matrix_list, list))
+
+    rows, columns = matrix.shape
+    entries = []
+    for r in range(rows):
+        for c in range(columns):
+            if np.abs(matrix[r, c]) > 1e-14:
+                entries.append([r+1, c+1, str(matrix[r, c])])
+    m = {"name": matrixname, "rows": rows, "columns": columns, "entries": entries}
+    matrix_list.append(m)
+    write_json(matrix_list, filename)
+
 
 def read_matrix(matrixname, filename):
     matrix_list = read_json(filename)
--- tests/test_json_io.py
+++ tests/test_json_io.py
@@ -8,6 +8,35 @@ from seissol_matrices import json_io
 
 
 class test_json_io(unittest.TestCase):
+    def test_write_read_one_matrix(self):
+        filename = "random.json"
+        original_name = "random"
+        original_matrix = np.random.rand(5, 5)
+        json_io.write_matrix(original_matrix, original_name, filename)
+        read_matrix = json_io.read_matrix(original_name, filename)
+        for i in range(original_matrix.shape[0]):
+            for j in range(original_matrix.shape[1]):
+                self.assertAlmostEqual(original_matrix[i, j], read_matrix[i, j])
+        os.remove(filename)
+
+    def test_write_read_several_matrices(self):
+        filename = "random.json"
+        original_names = ["random", "two", "three"]
+        original_matrix = [
+            np.random.rand(5, 5),
+            np.random.rand(2, 3),
+            np.random.rand(3, 2),
+        ]
+        for matrix, name in zip(original_matrix, original_names):
+            json_io.write_matrix(matrix, name, filename)
+
+        for matrix, name in zip(original_matrix, original_names):
+            read_matrix = json_io.read_matrix(name, filename)
+            for i in range(matrix.shape[0]):
+                for j in range(matrix.shape[1]):
+                    self.assertAlmostEqual(matrix[i, j], read_matrix[i, j])
+        os.remove(filename)
+
     def test_read_one_matrix(self):
         filename = "tests/test.json"
         original_name = "test"
