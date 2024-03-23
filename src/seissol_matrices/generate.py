bbeb25feca5e48f8924346e49f6b2a3903bcd672
David Schneller <david.schneller@tum.de>
2024-03-24 00:14:20 +0100

    Update VTK matrices to generate

--- src/seissol_matrices/generate.py
+++ src/seissol_matrices/generate.py
@@ -14,20 +14,25 @@ def main():
         for deg in range(0, 8):
             vtkpoints2[deg] = vtk_lagrange_2d(deg)
             vtkpoints3[deg] = vtk_lagrange_3d(deg)
-            json_io.write_matrix(vtkpoints2[deg], f"vtk2d{deg}", f"vtkbase.json")
-            json_io.write_matrix(vtkpoints3[deg], f"vtk3d{deg}", f"vtkbase.json")
+            json_io.write_matrix(vtkpoints2[deg].T, f"vtk2d({deg})", f"vtkbase.json")
+            json_io.write_matrix(vtkpoints3[deg].T, f"vtk3d({deg})", f"vtkbase.json")
         for basisorder in range(2, 8):
             dggen = dg_generator(basisorder, 3)
             for deg in range(0, 8):
                 json_io.write_matrix(
-                    dggen.collocate_volume(vtkpoints3[deg]),
-                    f"coll{basisorder}vd{deg}",
+                    dggen.collocate_volume(vtkpoints3[deg]).T,
+                    f"collvv({basisorder},{deg})",
+                    f"vtko{basisorder}.json",
+                )
+                json_io.write_matrix(
+                    dggen.face_generator.collocate_volume(vtkpoints2[deg]).T,
+                    f"collff({basisorder},{deg})",
                     f"vtko{basisorder}.json",
                 )
                 for f in range(4):
                     json_io.write_matrix(
-                        dggen.collocate_face(vtkpoints2[deg], f),
-                        f"coll{basisorder}f{f}d{deg}",
+                        dggen.collocate_face(vtkpoints2[deg], f).T,
+                        f"collvf({basisorder},{deg},{f})",
                         f"vtko{basisorder}.json",
                     )
 
